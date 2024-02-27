1cf295745a8096ab88bceef38e183a232d5fa4ac
Moises Delgadillo <moisesdelgadillowebsites@gmail.com>
2024-02-27 12:20:22 -0600

    Uso de \n (Salto de linea) y \t (Tabulacion)

---
+++ 006_Strings_nyt_Ejercicio.py
@@ -0,0 +1,11 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+#Los comentarios de arriba sirven para poder usar acentos en el código
+
+lista = "\tLista de Lenguajes de Programación\n- Python\n- JavaScript\n- Java\n- PHP\n- TypeScript\n- SQL\n- COBOL"
+print(lista)
