bba1c854c03f71d8a6256b69786904195f5e8d22
Moises Delgadillo <moisesdelgadillowebsites@gmail.com>
2024-02-27 11:05:08 -0600

    Uso de variables en python

---
+++ 002_Variables_Ejercicio.py
@@ -0,0 +1,9 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+mensaje = "Hola mundo" #La variable mensaje guarda el texto
+n1 = 10 #La variable n1 guarda el numero 10
+n2 = 20 #La variable n2 guarda el numero 20
+r = n1 + n2 #La variable r guarda la suma de n1 y n2
+
+print(r) #Se imrpime el resultado de la suma
