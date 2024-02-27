bcab37e4bb4b25c553f4bb7070a821d07c99556c
Moises Delgadillo <moisesdelgadillowebsites@gmail.com>
2024-02-27 11:53:53 -0600

    Uso de metodos upper(), title() y lowe()

---
+++ 005_Strings_Title_Ejercicio.py
@@ -0,0 +1,11 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+#Los comentarios de arriba sirven para poder usar acentos en el código
+
+nombre = "enrique barros fernández"
+print(nombre.title())
