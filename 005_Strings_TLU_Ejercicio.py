cc8f99c6325c790e10777101f246eb2317dbae41
Moises Delgadillo <moisesdelgadillowebsites@gmail.com>
2024-02-27 12:30:46 -0600

    Uso de metodos title(), lower() y upper()

---
+++ 005_Strings_TLU_Ejercicio.py
@@ -0,0 +1,17 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+#Los comentarios de arriba sirven para poder usar acentos en el código
+
+v1 = "enrique barros fernández"
+print(v1.title()) #Se usa para poner todas las primeras letras en mayuscula
+
+v1 = "Esta Es Una Frase Para Ser Formateada."
+print(v1.lower()) #Se usa para poner todas las letras en minusculas
+
+v1 = "Esta Es Una Frase Para Ser Formateada."
+print(v1.upper()) #Se usa para poner todas las letras en mayuscula
