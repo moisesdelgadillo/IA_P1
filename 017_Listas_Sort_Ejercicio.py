f29ee5ed447b673a65f5686907f6c923f1cc67f0
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 12:16:14 -0600

    Metodos sort(), sorted() y sort(reverse=True)

---
+++ 017_Listas_Sort_Ejercicio.py
@@ -0,0 +1,11 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(sorted(colores)) #El metodo sorted() ordena alfabeticamente la lista pero no se queda permamentemente
+print(colores)
+colores.sort() #El metodo sort() hace que se ordenen alfabeticamente de A a Z las listas
+print(colores)
+colores.sort(reverse=True) #El metodo sort(reverse=True) hace que se ordenen alfabeticamente de Z a A las listas
+print(colores)
