3569b6f9d17e70512b68ba88116c95379a38bb50
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 00:26:13 -0600

    Uso de metodo remove() para eliminar datos de listas por su nombre

---
+++ 013_Listas_Remove_Ejercicio.py
@@ -0,0 +1,8 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.remove('amarillo') #Se eliminaron los colores 'amarillo', 'rojo'
+colores.remove('rojo')
+print(colores)
+
