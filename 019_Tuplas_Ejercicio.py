1ac034ec7b7d942e2a9029c50e7f4203e24f9e70
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 12:29:43 -0600

    Uso de tuplas y diferencias con listas

---
+++ 019_Tuplas_Ejercicio.py
@@ -0,0 +1,12 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Las tuplas son similares a las listas solo que se definen con () en vez de [] y aparte
+#no se pueden modificar, es por ello que no se pueden ni quitar ni modificar su contenido
+#sería como una variable constante
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(colores[1])
+
+numeros = (10, 1, 5, 11)
+print(numeros[0] - numeros[1] + numeros[2] + numeros[3])#Igual se pueden sumar al ser numeros
