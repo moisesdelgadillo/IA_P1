5339f90e802c937400f7edf65b143d4bc7a7ad4e
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 12:53:34 -0600

    Uso de Else por si no se cumple la condicion If

---
+++ 022_Condicionales_IfElse_Ejercicio.py
@@ -0,0 +1,26 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Nombre del operador        Descripcion
+#       >                   Mayor que
+#       <                   Menor que
+#       >=                  Mayor o igual que
+#       <=                  Menor o igual que
+#       ==                  Igual que (comparando)
+#       !=                  Diferente que (comparando)
+
+#Ejercicios de python
+#Corrige el siguiente condicional if else.
+
+#color = rojo
+#else color == rojo
+#Print "El color es rojo."
+#if color != rojo
+#Print "El color no es rojo."
+
+color = "rojo"
+
+if color == "rojo":
+    print("El color es rojo.")
+else :
+    print("El color no es rojo.")
