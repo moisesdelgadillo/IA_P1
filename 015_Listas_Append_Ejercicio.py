13f643feead72b7e19f33b6df225cba166cc93f9
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 11:59:21 -0600

    Uso de metodo append() para agregar datos al final de la lista

---
+++ 015_Listas_Append_Ejercicio.py
@@ -0,0 +1,8 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.append("fuxia") #El metodo append hace que se agregue un elemento a la lista hasta el final
+colores.append("celeste")
+print(colores)
+
