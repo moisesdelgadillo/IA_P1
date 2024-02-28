1d4df6d6ad1ebc9d2d5fbdd64ac6ffd1d58549d8
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-27 22:58:33 -0600

    Uso de operadores Suma, Resta, Multiplicacion y Division

---
+++ 007_Operadores_srmd_Ejercicio.py
@@ -0,0 +1,11 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+suma = 20 + 23 + 44
+resta = 20 - 23 - 84
+multiplicacion = 10 * 87
+division = 5000 / 230 / 2
+operacion = (10 / 5) + 15 - 17
+
+print(suma, resta, multiplicacion, division)
+print(operacion)
