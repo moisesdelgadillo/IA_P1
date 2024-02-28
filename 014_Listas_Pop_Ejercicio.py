5b4cd33a516ef32aae95236392ff3b72cd20663d
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 00:33:33 -0600

    Eliminar un dato de una lista con pop() y agregarlo en otra variable

---
+++ 014_Listas_Pop_Ejercicio.py
@@ -0,0 +1,9 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores_eliminados = [colores.pop(1), colores.pop(-2)]
+
+print(colores)
+print(colores_eliminados)
+
