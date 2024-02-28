0570d1feebdf2cbf52c7e5c9efc6d298b7d34046
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 00:11:37 -0600

    Uso de listas

---
+++ 010_Listas_Ejercicio.py
@@ -0,0 +1,10 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+#¿En qué posición se encuentra el color 'rojo'? ¿y el 'rosa'?
+#En la posicion 0 el rojo, y 7 el rosa
+print(colores[0], colores[7])
+
+numeros = ["tres", "dos", "cinco", "cuatro", "uno"]
+print(numeros[4], numeros[1], numeros[0], numeros[3], numeros[2])
