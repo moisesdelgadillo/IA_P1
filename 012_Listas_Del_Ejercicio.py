0826bb171e808ff78bffe1661b56fca526d27268
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 00:21:53 -0600

    Uso de metodo del para eliminar elementos en listas

---
+++ 012_Listas_Del_Ejercicio.py
@@ -0,0 +1,7 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+del colores[1], colores[4], colores[-4], colores[-3] #Se eliminaron los colores 'azul', 'marrón', 'negro' y 'rosa'
+print(colores)
+
