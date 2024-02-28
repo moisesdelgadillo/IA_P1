5faf0f8db8254f823f610c9394958cfa592328e3
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 12:35:16 -0600

    Convertir tuplas a listas o listas a tuplas

---
+++ 020_Tuplas_Convertir_Ejercicio.py
@@ -0,0 +1,9 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Si quieres convertir una lista a tupla deberas usar el metodo tuple(lista)
+#Si quieres convertir una tupla a lista deberas usar el metodo list(tupla)
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+tupla_colores = tuple(colores) #Aqui converti la lista colores a una tupla
+print(tupla_colores)
