bfaedcbe83b665101ea259e2a90e3400b348d2df
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 00:17:06 -0600

    Uso de Posiciones negativas para acceder a datos en listas

---
+++ 011_Listas_PN_Ejercicio.py
@@ -0,0 +1,8 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#¿En qué posición se encuentra el color 'rojo'? ¿y el 'rosa'?
+#En la posicion 0 el rojo, y 7 el rosa
+print(colores[-1], colores[3], colores[-5], colores[-2], colores[0])
+
