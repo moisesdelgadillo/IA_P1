28af7de6c4ce73db3b0f492e719ed0b6f96da656
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 12:48:12 -0600

    Uso de condicional IF y sus operadores

---
+++ 021_Condicionales_If_Ejercicio.py
@@ -0,0 +1,32 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Nombre del operador        Descripcion
+#       >                   Mayor que
+#       <                   Menor que
+#       >=                  Mayor o igual que
+#       <=                  Menor o igual que
+#       ==                  Igual que (comparando)
+#       !=                  Diferente que (comparando)
+
+#Ejercicios de python
+#Cambia el operador para que la condición sea True
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+	print('Se ejecuta el if NO.1.')
+
+#Cambia el operador para que la condición sea True
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if NO.2.')
+
+#Haz que el siguiente condicional se convierta en False sin cambiar el operador
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')
