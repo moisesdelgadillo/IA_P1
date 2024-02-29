75771cc48aa3bdf933712d92656cffd76566017c
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 22:24:36 -0600

    Uso de bucles while

---
+++ 027_While_Ejercicio.py
@@ -0,0 +1,27 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5
+x = 0
+while x <= 15:
+    print(x)
+    x+=5
+
+print("\n\n")
+#Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20
+x = 0
+while x >=- 100:
+    print(x)
+    x-=20
+
+print("\n\n")
+#Crea un bucle while que se ejecute hasta que x valga 0 con decrementos de 1 y que
+#muestre en cada ejecución esta frase con el valor de ejecución correspondiente:
+#'El valor del bucle es 10'
+
+x = 10
+while x >= 0:
+    print("El valor del bucle es ",x)
+    x-=1
+
+print("\n\n")
