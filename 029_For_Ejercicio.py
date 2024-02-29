d3618f63128c129ff86621babe7a10bacda12786
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 23:05:05 -0600

    Uso de bucle for

---
+++ 029_For_Ejercicio.py
@@ -0,0 +1,9 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Crea un bucle for que itere la siguiente tupla y muestre una frase como esta
+#en cada iteración: 'El color es: ' + color + '.'.
+
+colores = ('rojo','azul','verde','amarillo')
+for x in colores:  #Con el for al iterarlo con in y al ser una lista o tupla lo ira iterando de una en una
+    print("El color es: ", x, ".")
