77bb77fc56080a048ab1c89e47e6417d687c38c8
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-29 13:25:10 -0600

    Uso de excepciones (try, except)

---
+++ 051_Excepciones_Ejercicio.py
@@ -0,0 +1,14 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#El bloque try:(prueba en español) permite probar un bloque de código en busca de errores.
+#El bloque except: permite manejar errores en caso de que estos ocurran.
+
+try:
+	print(variable) #Al querer imprimir una variable que no se ha declarado puede
+	#surgir un error, pero al usar el try puede agarrar ese error y hacer que no truene el
+	#codigo
+	
+except:
+	print("La variable no está declarada.") #Y al agarrar un error se ejecutará el except
+	
