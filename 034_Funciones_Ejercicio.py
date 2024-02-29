9c1cb2f2f5d659091225a1d19a451792b1da1afd
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-29 00:17:57 -0600

    Uso de funciones

---
+++ 034_Funciones_Ejercicio.py
@@ -0,0 +1,21 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+def saludo():
+	print("Bienvenidos a este codigo")
+
+saludo()
+
+#Crea una función que realice una suma. Para ello, tendrás que añadir
+#dos argumentos(numero1 y numero2). En su interior, especificarás un print()
+#que muestre el resultado de la suma. Deberás hacer tres llamadas que como
+#resultado de la suma den los valores 30, 50 y 57000. Los números que
+#introduzcas en la llamada pueden ser los que quieras siempre que coincidan
+#los resultados en el print().
+
+def suma(n1, n2):
+    print("Tu resultado es: ", n1 + n2)
+
+suma(10, 20)
+suma(30, 20)
+suma(50000, 7000)
