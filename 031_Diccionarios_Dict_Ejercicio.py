7caeb817a454e3cbcaf4dad5b7d679aac9d10e24
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 23:29:53 -0600

    Uso de diccionarios

---
+++ 031_Diccionarios_Dict_Ejercicio.py
@@ -0,0 +1,19 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio
+#con presentación en un print(). El resultado será esto en la consola:
+
+teclado1 = { #Esto es un diccionario -> (key): (value)
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print("El modelo", teclado2["Modelo"], "cuesta", teclado2["Precio"], "$.")
