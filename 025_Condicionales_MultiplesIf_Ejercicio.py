f9fe8a15aab7e6d9809aa6cc76b1687d453f564a
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 21:50:01 -0600

    Uso de multiples if

---
+++ 025_Condicionales_MultiplesIf_Ejercicio.py
@@ -0,0 +1,13 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+entrada = int(input('Introduce un número del 1 al 4:\n'))
+
+if entrada == 1:
+    print('Has seleccionado la opción número 1.')
+if entrada == 2:
+    print('Has seleccionado la opción número 2.')
+if entrada == 3:
+    print('Has seleccionado la opción número 3.')
+if entrada == 4:
+    print('Has seleccionado la opción número 4.')
