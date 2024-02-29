5ed2ac46a1c75743078a9caa3983023725e58596
moisesdelgadillo <108106054+moisesdelgadillo@users.noreply.github.com>
2024-02-28 23:18:32 -0600

    Uso de metodo range() en bucle for

---
+++ 030_For_Range_Ejercicio.py
@@ -0,0 +1,8 @@
+#ERICK MOISES DELGADILLO LARA
+#21310139 - 6E1
+
+#Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700
+#en saltos de 100. Basta con que imprimas el valor de cada iteración.
+
+for x in range(7,700,100): #range(inicio, condicion "hasta cuando", incremento/decremento)
+    print(x)
